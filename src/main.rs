f894c2e5b9b5dc606d790a7aadb1ec57c19eddfa
Luan Baleeiro <dev.baleeiro@gmail.com>
2025-11-17 19:29:43 -0300

    configuração de layout workspace

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    
+    // load .env file
+    dotenvy::dotenv().expect("Failed to load .env file");
+
+    // start logger (tracing)
+    tracing_subscriber::fmt::init();
+    tracing::info!("Starting server application...");
 }
